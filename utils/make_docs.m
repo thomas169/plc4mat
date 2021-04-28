7e11aeaa90b1abc9089889441a42fc72021fc4a0
thomas <frost.tj@protonmail.com>
2021-04-28 11:58:34 +0100

    final docs update

--- utils/make_docs.m
+++ utils/make_docs.m
@@ -3,12 +3,16 @@ function make_docs(recipe)
 readmeSrc = 'readme.adoc';
 adocSrc = {readmeSrc};
 docsDir = 'docs';
-projectRoot = fullfile(strrep(mfilename('fullpath'),mfilename,''),'..');
+imgDir = 'resources';
+projectRoot = regexprep(fullfile(strrep(mfilename('fullpath'),mfilename,'')),'[^/]+/$','');
 outDir = fullfile(projectRoot, docsDir);
+imgsDir =  fullfile(projectRoot, docsDir, imgDir);
+
+resDir = '..';
 
 for n = 1 : numel(adocSrc)
     file = fullfile(projectRoot, docsDir, adocSrc{n});
-    makeCmd = sprintf('asciidoctor %s -D %s', file, outDir);
+    makeCmd = sprintf('asciidoctor %s -D %s -a imagesdir=%s', file, outDir, resDir);
     system(makeCmd);
 end
 
